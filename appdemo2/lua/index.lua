709d260fcef91088e2a96a2674020170ad651a21
jannson <gandancing@gmail.com>
2018-06-14 08:39:24 +0800

    web support for appdemo2

--- appdemo2/lua/index.lua
+++ appdemo2/lua/index.lua
@@ -1,12 +1,84 @@
+local nixio = require "nixio"
+local dbus = require "luci.dbus"
+local json = require "luci.json"
+local http = require "luci.http"
+local util  = require "luci.util"
 local ksutil = require "luci.ksutil"
+local httpclient = require "luci.httpclient"
 
 module("luci.controller.apps.appdemo2.index", package.seeall)
 
 function index()
 	entry({"appdemo2"}, call("action_appdemo2"))
+	entry({"appdemo2", "status"}, call("action_appdemo2_status"))
+	entry({"appdemo2", "https-test"}, call("action_https"))
+	entry({"appdemo2", "mount-test"}, call("action_mount"))
+	-- error("string expected")
 end
 
 function action_appdemo2()
     ksutil.shell_action("appdemo2")
+end
+
+function action_appdemo2_status()
+    http.prepare_content("text/plain; charset=utf-8")
+    reader = ksutil.popen("/koolshare/scripts/appdemo2-config.sh status", nil)
+    luci.ltn12.pump.all(reader, luci.http.write)
+end
+
+function action_https()
+    forms = http.formvalue()
+    for k,v in pairs(forms) do
+        util.perror("k: " .. k .. " v: " .. tostring(v))
+    end
+	local b = httpclient.request_to_buffer("https://koolshare.ngrok.wang/softcenter/config.json.js")
+	http.prepare_content("application/json")
+	http.write(b)
+end
+
+function action_mount()
+	local data = {}
+	local k = {"fs", "blocks", "used", "available", "percent", "mountpoint"}
+	local ps = util.execi("df")
+
+	if not ps then
+		return
+	else
+		ps()
+	end
+
+	for line in ps do
+		local row = {}
+
+		local j = 1
+		for value in line:gmatch("[^%s]+") do
+			row[k[j]] = value j = j + 1
+		end
+
+		if row[k[1]] then
+
+			-- this is a rather ugly workaround to cope with wrapped lines in
+			-- the df output:
+			--
+			--	/dev/scsi/host0/bus0/target0/lun0/part3
+			--                   114382024  93566472  15005244  86% /mnt/usb
+			--
+
+			if not row[k[2]] then
+				j = 2
+				line = ps()
+				for value in line:gmatch("[^%s]+") do
+					row[k[j]] = value
+					j = j + 1
+				end
+			end
+
+			table.insert(data, row)
+		end
+	end
+
+    http.prepare_content("application/json")
+    local js = json.encode(data)
+    http.write(js)
 end
 
--- appdemo2/scripts/config.sh
+++ appdemo2/scripts/config.sh
@@ -1,27 +1,87 @@
 #!/bin/bash
 
 . /koolshare/scripts/base.sh
+. /koolshare/scripts/jshn.sh
 . /koolshare/scripts/uci.sh
 
-on_installed() {
-    app_init_cfg '{"appdemo2":[{"_id":"main","status":"not-init","enabled":"0", "token":" "}]}'
-    app_log "installed"
-}
+on_get() {
+    appdemo2_status=`pidof appdemo2|wc -w`
 
-on_start() {
-    echo 'started'
-}
+    INPUT_JSON=`uci export_json appdemo2`
+    json_load "$INPUT_JSON"
+    json_select "appdemo2"
+    json_select "1"
 
-on_stop() {
-    echo 'stoped'
+    if [ "$appdemo2_status"x = "2"x ];then
+        json_add_string "status" "start"
+    else
+        json_add_string "status" "stop"
+    fi
+    json_dump
 }
 
 on_post() {
-    echo '{"status":"ok", "description":"on_post"}'
+    local appdemo2_enabled
+    local appdemo2_token
+
+    json_load "$INPUT_JSON"
+    json_select "appdemo2"
+    json_select "1"
+    json_get_var appdemo2_enabled "enabled"
+    json_get_var appdemo2_token "token"
+
+    app_log $appdemo2_enabled
+    app_log $appdemo2_token
+    if [ "$appdemo2_enabled"x = "1"x ]; then
+        killall appdemo2 > /dev/null 2>&1
+        $APP_ROOT/bin/appdemo2 -u $appdemo2_token -d > /dev/null 2>&1
+        json_add_string "status" ""
+        json_dump|app_save_cfg
+
+        # mark as start
+        json_add_string "status" "start"
+        json_dump
+    elif [ "$appdemo2_enabled"x = "0"x ]; then
+        killall appdemo2 > /dev/null 2>&1
+        json_add_string "status" ""
+        json_dump|app_save_cfg
+
+        # mark as stop
+        json_add_string "status" "stop"
+        json_dump
+    else
+        echo '{"status": "json_parse_failed"}'
+    fi
 }
 
-on_get() {
-    echo '{"status":"ok", "description":"on_get"}'
+on_start() {
+    local appdemo2_enabled
+    local appdemo2_token
+    config_load appdemo2
+    config_get appdemo2_enabled main enabled
+    config_get appdemo2_token main token
+    if [ "$appdemo2_enabled"x = "1"x ]; then
+        killall appdemo2 > /dev/null 2>&1
+        $APP_ROOT/bin/appdemo2 -u $appdemo2_token -d > /dev/null 2>&1
+    else
+        killall appdemo2 > /dev/null 2>&1
+    fi
+}
+
+on_stop() {
+    killall appdemo2 > /dev/null 2>&1
+}
+
+on_status() {
+    appdemo2_status=`pidof appdemo2|wc -w`
+    appdemo2_pid=`pidof appdemo2`
+    appdemo2_version=`$APP_ROOT"/bin/appdemo2" -v`
+    appdemo2_route_id=`$APP_ROOT"/bin/appdemo2" -w | awk '{print $2}'`
+    if [ "$appdemo2_status"x = "2"x ];then
+        echo 进程运行正常！版本：${appdemo2_version} 路由器ID：${appdemo2_route_id} （PID：$appdemo2_pid）
+    else
+        echo \<em\>【警告】：进程未运行！\<\/em\> 版本：${appdemo2_version} 路由器ID：${appdemo2_route_id}
+    fi
 }
 
 case $ACTION in
@@ -35,16 +95,15 @@ get)
     on_get
     ;;
 installed)
-    on_installed
+    app_init_cfg '{"appdemo2":[{"_id":"main","status":"not-init","enabled":"0", "token":" "}]}'
+    ;;
+status)
+    on_status
     ;;
 stop)
     on_stop
-    ;;
-help)
-    echo `basename $0` 'start/post/get/installed/stop'
     ;;
 *)
     on_start
     ;;
 esac
-
